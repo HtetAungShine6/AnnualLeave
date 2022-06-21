7cc70be5a42261a8455c506fae115a94a794f515
Htet Aung Shine <htetaungshine211299@gmail.com>
2022-06-21 17:01:40 +0630

    added delete feature

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 32
     defaultConfig {
         applicationId = "com.example.navbar.android"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 32
         versionCode = 1
         versionName = "1.0"
--- androidApp/src/main/java/com/example/navbar/android/fragments/list/ListFragment.kt
+++ androidApp/src/main/java/com/example/navbar/android/fragments/list/ListFragment.kt
@@ -1,9 +1,9 @@
 package com.example.navbar.android.fragments.list
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -40,7 +40,34 @@ class ListFragment : Fragment() {
         view.floatingActionButton.setOnClickListener(){
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete){
+            deleteUser()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteUser(){
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){ _ , _ ->
+            mUserViewModel.deleteAllUser()
+            Toast.makeText(this.requireContext(),"Successfully Deleted Everything", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No"){ _ , _ ->
+
+        }
+        builder.setTitle("Delete Everything?")
+        builder.setMessage("Are you sure you want to delete everything?")
+        builder.create().show()
     }
 
 }
